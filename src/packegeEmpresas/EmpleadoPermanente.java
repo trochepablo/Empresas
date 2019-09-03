2d6574848d4a5dee7ae62e3210ce9b82c6651574
nikcolastancredi <nikco_tancredi@hotmail.com>
2019-09-02 23:17:17 -0300

    Commit prueba

--- src/packegeEmpresas/EmpleadoPermanente.java
+++ src/packegeEmpresas/EmpleadoPermanente.java
@@ -1,24 +1,59 @@
 package packegeEmpresas;
 
+import java.util.Date;
+
 public class EmpleadoPermanente extends Empleado {
-    Integer antiguedad;
-    Integer cantidadHijos;
+    private Integer antiguedad;
+    private Integer cantidadHijos;
+
+    //Getters & Setters
+    public Integer getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(Integer antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    public Integer getCantidadHijos() {
+        return cantidadHijos;
+    }
+
+    public void setCantidadHijos(Integer cantidadHijos) {
+        this.cantidadHijos = cantidadHijos;
+    }
+
+    //Constructor
+    public EmpleadoPermanente(String nombre, String direccion, Boolean estaCasado,
+                              Date fechaNacimiento, Double sueldoBasico, Integer antiguedad, Integer cantidadHijos) {
+        super(nombre, direccion, estaCasado, fechaNacimiento, sueldoBasico);
+        this.antiguedad = antiguedad;
+        this.cantidadHijos = cantidadHijos;
+    }
+
+    //Method
+    @Override
+    public Double getSueldoBruto() {
+        return (this.getSueldoBasico() + this.getSalarioFamilar() + this.getSalarioAntiguedad()); }
 
     @Override
-    public Float sueldoBruto() { return (this.sueldoBasica + SalarioFamiliar() + SalarioAntiguedad()); }
-    @Override
-    public Float retenciones() { return RetObraSocial() + RetAporteJubilatorio(); }
+    public Double getRetenciones() { return this.getRetencionesObraSocial() + this.getReportesAportesJubilatorio(); }
 
-    private Float RetAporteJubilatorio() { return sueldoBruto() % 15; }
-    private Float RetObraSocial() { return (sueldoBruto() % 10) + 20 * cantidadHijos; }
-    private Float SalarioAntiguedad() {
-        return (float) (50 * antiguedad);
-    }
-    private Float SalarioFamiliar(){
-        return (float) (150 * cantidadHijos + (this.estadoCivil ? 100 : 0));
-    }
+    private Double getReportesAportesJubilatorio() { return this.getSueldoBruto() % 15; }
+    private Double getRetencionesObraSocial() { return (double) (this.getSueldoBruto() % 10) + (20 * this.getCantidadHijos()); }
 
-    public Float sueldoNeto(){
-        return 1f;
+    private Double getSalarioFamilar() {
+            if (this.getEstaCasado()) {
+                 return (double) (this.getCantidadHijos() * 150)  + 100);
+            } else{
+                return (double) (this.getCantidadHijos() * 150);
+            }
     }
+    private Double getSalarioAntiguedad(){
+        return (double) (50 * this.getAntiguedad());
+    }
+    public Double getSueldoNeto(){return 1d;}
+
+    // estoy haciendo un cambio
+
 }
